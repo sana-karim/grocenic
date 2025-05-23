faa2c2c9de62a2684c95d7edf69aa6b386023aa8
sana-karim <sanakarim4@gmail.com>
2025-05-23 15:08:24 +0530

    added home screen

---
+++ src/screens/home/Home.screen.tsx
@@ -0,0 +1,120 @@
+import { NavigationProp } from '@react-navigation/native';
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+import { ScreenLayout } from '../../components/screen_layout/ScreenLayout';
+import { HomeHeader } from '../../components/headers/HomeHeader';
+
+interface HomeProps {
+    navigation?: NavigationProp<any>;
+}
+
+export const Home: React.FC<HomeProps> = ({ navigation }) => {
+
+    return (
+        <ScreenLayout headerComponent={<HomeHeader />}>
+            <ScrollView>
+                <Text>Hello this is Home Screen 1</Text>
+                <Text>Hello this is Home Screen 2</Text>
+                <Text>Hello this is Home Screen 3</Text>
+                <Text>Hello this is Home Screen 4</Text>
+                <Text>Hello this is Home Screen 5</Text>
+                <Text>Hello this is Home Screen 6</Text>
+                <Text>Hello this is Home Screen 7</Text>
+                <Text>Hello this is Home Screen 8</Text>
+                <Text>Hello this is Home Screen 9</Text>
+                <Text>Hello this is Home Screen 10</Text>
+                <Text>Hello this is Home Screen 11</Text>
+                <Text>Hello this is Home Screen 12</Text>
+                <Text>Hello this is Home Screen 13</Text>
+                <Text>Hello this is Home Screen 14</Text>
+                <Text>Hello this is Home Screen 15</Text>
+                <Text>Hello this is Home Screen 16</Text>
+                <Text>Hello this is Home Screen 17</Text>
+                <Text>Hello this is Home Screen 18</Text>
+                <Text>Hello this is Home Screen 19</Text>
+                <Text>Hello this is Home Screen 20</Text>
+                <Text>Hello this is Home Screen 21</Text>
+                <Text>Hello this is Home Screen 22</Text>
+                <Text>Hello this is Home Screen 23</Text>
+                <Text>Hello this is Home Screen 24</Text>
+                <Text>Hello this is Home Screen 25</Text>
+                <Text>Hello this is Home Screen 26</Text>
+                <Text>Hello this is Home Screen 27</Text>
+                <Text>Hello this is Home Screen 28</Text>
+                <Text>Hello this is Home Screen 29</Text>
+                <Text>Hello this is Home Screen 30</Text>
+                <Text>Hello this is Home Screen 31</Text>
+                <Text>Hello this is Home Screen 32</Text>
+                <Text>Hello this is Home Screen 33</Text>
+                <Text>Hello this is Home Screen 34</Text>
+                <Text>Hello this is Home Screen 35</Text>
+                <Text>Hello this is Home Screen 36</Text>
+                <Text>Hello this is Home Screen 37</Text>
+                <Text>Hello this is Home Screen 38</Text>
+                <Text>Hello this is Home Screen 39</Text>
+                <Text>Hello this is Home Screen 40</Text>
+                <Text>Hello this is Home Screen 41</Text>
+                <Text>Hello this is Home Screen 42</Text>
+                <Text>Hello this is Home Screen 43</Text>
+                <Text>Hello this is Home Screen 44</Text>
+                <Text>Hello this is Home Screen 45</Text>
+                <Text>Hello this is Home Screen 46</Text>
+                <Text>Hello this is Home Screen 47</Text>
+                <Text>Hello this is Home Screen 48</Text>
+                <Text>Hello this is Home Screen 49</Text>
+                <Text>Hello this is Home Screen 50</Text>
+                <Text>Hello this is Home Screen 51</Text>
+                <Text>Hello this is Home Screen 52</Text>
+                <Text>Hello this is Home Screen 53</Text>
+                <Text>Hello this is Home Screen 54</Text>
+                <Text>Hello this is Home Screen 55</Text>
+                <Text>Hello this is Home Screen 56</Text>
+                <Text>Hello this is Home Screen 57</Text>
+                <Text>Hello this is Home Screen 58</Text>
+                <Text>Hello this is Home Screen 59</Text>
+                <Text>Hello this is Home Screen 60</Text>
+                <Text>Hello this is Home Screen 61</Text>
+                <Text>Hello this is Home Screen 62</Text>
+                <Text>Hello this is Home Screen 63</Text>
+                <Text>Hello this is Home Screen 64</Text>
+                <Text>Hello this is Home Screen 65</Text>
+                <Text>Hello this is Home Screen 66</Text>
+                <Text>Hello this is Home Screen 67</Text>
+                <Text>Hello this is Home Screen 68</Text>
+                <Text>Hello this is Home Screen 69</Text>
+                <Text>Hello this is Home Screen 70</Text>
+                <Text>Hello this is Home Screen 71</Text>
+                <Text>Hello this is Home Screen 72</Text>
+                <Text>Hello this is Home Screen 73</Text>
+                <Text>Hello this is Home Screen 74</Text>
+                <Text>Hello this is Home Screen 75</Text>
+                <Text>Hello this is Home Screen 76</Text>
+                <Text>Hello this is Home Screen 77</Text>
+                <Text>Hello this is Home Screen 78</Text>
+                <Text>Hello this is Home Screen 79</Text>
+                <Text>Hello this is Home Screen 80</Text>
+                <Text>Hello this is Home Screen 81</Text>
+                <Text>Hello this is Home Screen 82</Text>
+                <Text>Hello this is Home Screen 83</Text>
+                <Text>Hello this is Home Screen 84</Text>
+                <Text>Hello this is Home Screen 85</Text>
+                <Text>Hello this is Home Screen 86</Text>
+                <Text>Hello this is Home Screen 87</Text>
+                <Text>Hello this is Home Screen 88</Text>
+                <Text>Hello this is Home Screen 89</Text>
+                <Text>Hello this is Home Screen 90</Text>
+                <Text>Hello this is Home Screen 91</Text>
+                <Text>Hello this is Home Screen 92</Text>
+                <Text>Hello this is Home Screen 93</Text>
+                <Text>Hello this is Home Screen 94</Text>
+                <Text>Hello this is Home Screen 95</Text>
+                <Text>Hello this is Home Screen 96</Text>
+                <Text>Hello this is Home Screen 97</Text>
+                <Text>Hello this is Home Screen 98</Text>
+                <Text>Hello this is Home Screen 99</Text>
+                <Text>Hello this is Home Screen 100</Text>
+            </ScrollView>
+        </ScreenLayout>
+    )
+
+}
