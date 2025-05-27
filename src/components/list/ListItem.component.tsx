5cf3d901639def738c3400c51b254644dfede9ec
sana-karim <sanakarim4@gmail.com>
2025-05-28 03:57:37 +0530

    added option icon to list item

--- src/components/list/ListItem.component.tsx
+++ src/components/list/ListItem.component.tsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { GrocenicTheme } from "../../theme/GrocenicTheme";
 
 interface ListItemProps {
     itemLabel: string | number;
     quantity?: string | number;
+    onOptBtn?: any
 };
 
-export const ListItem: React.FC<ListItemProps> = ({ itemLabel, quantity }) => {
+export const ListItem: React.FC<ListItemProps> = ({ itemLabel, quantity, onOptBtn }) => {
     return (
         <View style={styles.container}>
-            <Text>{itemLabel}</Text>
-            <Text>{quantity}</Text>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>{itemLabel}</Text>
+                <Text style={styles.quantity}>{quantity}</Text>
+            </View >
+            <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => {
+                    //TODO: function called later
+                }}
+            >
+                <Image source={require('../../assets/images/more_option.png')} />
+            </TouchableOpacity>
         </View>
+
     )
 }
 
@@ -23,8 +35,22 @@ const styles = StyleSheet.create({
         marginVertical: GrocenicTheme.spacing.xs,
         backgroundColor: GrocenicTheme.colors.background,
         borderRadius: GrocenicTheme.borderRadius.md,
-        display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+    },
+    textContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingRight: GrocenicTheme.spacing.sm,
+        flex: 1,
+    },
+    text: {
+        fontSize: GrocenicTheme.fontSize.medium,
+        fontFamily: GrocenicTheme.fonts.medium,
+        color: GrocenicTheme.colors.textPrimary
+    },
+    quantity: {
+        fontSize: GrocenicTheme.fontSize.medium,
+        fontFamily: GrocenicTheme.fonts.medium,
+        color: GrocenicTheme.colors.textSecondary
     }
 })
