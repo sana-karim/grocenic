cea6217d9f61beb721b03bbcdbed0eeecd6adc46
sana-karim <sanakarim4@gmail.com>
2025-05-27 22:44:45 +0530

    splash screen hide when app load

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar, StyleSheet } from "react-native";
 import { ToastProvider } from 'react-native-toast-notifications';
 import MainNavigation from "./navigation/navigation";
 import { GrocenicTheme } from "./theme/GrocenicTheme";
+import BootSplash from "react-native-bootsplash";
 
 export const Main = () => {
+
+    useEffect(() => {
+        const init = async () => {
+            // …do multiple sync or async tasks
+        };
+
+        init().finally(async () => {
+            await BootSplash.hide({ fade: true });
+        });
+    }, []);
 
     return (
         <>
